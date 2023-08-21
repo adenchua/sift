a10599231377bb7d1a0ee42f79fad7f4c7b20b94
Aden <adenchuayouliang@gmail.com>
2023-08-21 21:28:39 +0800

    add channel feature

--- backend/server.py
+++ backend/server.py
@@ -7,6 +7,7 @@ from services.subscriber_service import (
     SubscriberService,
     SubscriptionException,
 )
+from services.channel_service import Channel, ChannelService
 
 app = FastAPI()
 
@@ -23,10 +24,10 @@ async def root():
 
 @app.post("/subscriber", status_code=201)
 async def add_new_subscriber(subscriber: Subscriber):
-    subscriber_client = SubscriberService()
+    subscriber_service = SubscriberService()
     subscriber_id = subscriber.telegram_id
     try:
-        response = subscriber_client.add_subscriber(subscriber=subscriber)
+        response = subscriber_service.add_subscriber(subscriber=subscriber)
         return {"message": f"New subscriber with id {response} created!"}
     except SubscriptionException:
         raise HTTPException(
@@ -37,28 +38,43 @@ async def add_new_subscriber(subscriber: Subscriber):
 
 @app.patch("/subscriber/{subscriber_id}/themes", status_code=204)
 async def update_subscriber_theme_keywords(subscriber_id: str, theme: SubscriberTheme):
-    subscriber_client = SubscriberService()
+    subscriber_service = SubscriberService()
 
-    subscriber_client.update_subscriber_keywords(
+    subscriber_service.update_subscriber_keywords(
         subscriber_id=subscriber_id, theme=theme.theme, new_keywords=theme.keywords
     )
 
 
 @app.post("/subscriber/{subscriber_id}/unsubscribe", status_code=204)
 async def unsubscribe(subscriber_id: str):
-    subscriber_client = SubscriberService()
+    subscriber_service = SubscriberService()
 
-    if not subscriber_client.check_subscriber_exists(subscriber_id):
+    if not subscriber_service.check_subscriber_exists(subscriber_id):
         raise HTTPException(status_code=404, detail=f"Subscriber record not found")
 
-    subscriber_client.toggle_subscription(subscriber_id=subscriber_id, is_subscribed=False)
+    subscriber_service.toggle_subscription(subscriber_id=subscriber_id, is_subscribed=False)
 
 
 @app.post("/subscriber/{subscriber_id}/subscribe", status_code=204)
 async def subscribe(subscriber_id: str):
-    subscriber_client = SubscriberService()
+    subscriber_service = SubscriberService()
 
-    if not subscriber_client.check_subscriber_exists(subscriber_id):
+    if not subscriber_service.check_subscriber_exists(subscriber_id):
         raise HTTPException(status_code=404, detail=f"Subscriber record not found")
 
-    subscriber_client.toggle_subscription(subscriber_id=subscriber_id, is_subscribed=True)
+    subscriber_service.toggle_subscription(subscriber_id=subscriber_id, is_subscribed=True)
+
+
+@app.post("/channels", status_code=201)
+async def add_channel(channel: Channel):
+    channel_service = ChannelService()
+    channel_id = channel.channel_id
+
+    try:
+        response = channel_service.add_channel(channel=channel)
+        return {"message": f"New channel with id {response} created!"}
+    except SubscriptionException:
+        raise HTTPException(
+            status_code=409,
+            detail=f"Channel with id <{channel_id}> already exists",
+        )
--- backend/services/channel_service.py
+++ backend/services/channel_service.py
@@ -18,6 +18,9 @@ class ChannelService:
     def __init__(self):
         self.database_client = DatabaseClient()
 
+    def check_channel_exists(self, channel_id: str):
+        return self.database_client.document_exist(index_name=self.__INDEX_NAME, document_id=channel_id)
+
     def get_channels(self):
         """
         Retrieves all channels in the database, including inactive channels
@@ -27,6 +30,16 @@ class ChannelService:
             return result
         except Exception as error:
             raise (error)
+
+    def get_channel(self, channel_id: str) -> Channel:
+        try:
+            response = self.database_client.read(index_name=self.__INDEX_NAME, query={"match": {"_id": channel_id}})
+            if len(response) == 1:
+                return response[0]
+
+            return None
+        except Exception as error:
+            raise error
 
     def get_active_channels(self):
         """
@@ -57,8 +70,18 @@ class ChannelService:
         """
         Adds a new channel to the database. A channel's is_active flag is set to True by default
         unless specified.
+
+        If a channel exists already, add the theme to existing channel instead
         """
         try:
+            existing_channel = self.get_channel(channel_id=channel.channel_id)
+            # channel exists, add theme to channel
+            if existing_channel is not None:
+                # ensure no duplicates of themes
+                combined_themes = list(set().union([*existing_channel["themes"], *channel.themes]))
+                self.update_channel_themes(channel_id=channel.channel_id, themes=combined_themes)
+                return channel.channel_id
+
             result = self.database_client.create(
                 index_name=self.__INDEX_NAME,
                 document={
@@ -67,11 +90,11 @@ class ChannelService:
                     "offset_id": channel.offset_id,
                     "themes": channel.themes,
                 },
-                document_id=channel.channel_id,
+                document_id=channel.channel_id.lower(),  # force lowercase on channel id
             )
             return result
         except Exception as error:
-            raise (error)
+            raise error
 
     def toggle_channel_activeness(self, channel_id: str, is_active: bool) -> bool:
         """
