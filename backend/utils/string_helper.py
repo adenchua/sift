54c43ffc75e4ffa0be7c4fff89d5e0c55b70f824
Aden <adenchuayouliang@gmail.com>
2023-09-23 09:59:38 +0800

    update subscriber mapping

--- backend/utils/string_helper.py
+++ backend/utils/string_helper.py
@@ -2,6 +2,6 @@ import re
 
 
 def clean_string(input_string: str) -> str:
-    # Remove all non-alphanumeric characters except commas
-    cleaned_string = re.sub("[^a-zA-Z0-9,]", "", input_string)
+    # Remove all non-alphanumeric characters except commas and spaces in between
+    cleaned_string = re.sub("[^a-zA-Z0-9, ]", "", input_string).strip()
     return cleaned_string
