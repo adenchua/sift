d05ee253a4c291006d0e5c68d226d37f43fe3289
Aden <adenchuayouliang@gmail.com>
2023-12-13 07:48:26 +0800

    add background service script and docker

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import argparse
 import asyncio
 
-from services.subscriber_service import SubscriberService
 from services.channel_service import ChannelService
 from services.message_service import MessageService
-from utils.message_helper import extract_and_ingest_messages
-from utils.date_helper import get_latest_iso_datetime
+from services.subscriber_service import SubscriberService
 from services.telegram_bot_client import TelegramBotClient
+from utils.date_helper import get_latest_iso_datetime
+from utils.message_helper import extract_and_ingest_messages
 
 
 async def send_message_from_bot(message: str, receiver_chat_id: str):
@@ -34,9 +34,7 @@ async def notify_subscribers():
             timestamp = subscribed_theme["last_crawl_timestamp"]
             message_iso_dates = []  # datetime for comparison later
 
-            messages = message_service.get_messages(
-                keywords_list=keywords, theme=theme, iso_date=timestamp
-            )
+            messages = message_service.get_messages(keywords_list=keywords, theme=theme, iso_date=timestamp)
 
             for message in messages:
                 message_iso_dates.append(message["timestamp"])
@@ -69,10 +67,29 @@ async def download_telegram_messages():
         )
 
 
+async def start_background_download_service():
+    """
+    runs the crawling of messages on a set interval
+    """
+    while True:
+        await download_telegram_messages()
+        await asyncio.sleep(60 * 15)  # download messages every 15 minutes
+
+
+async def start_background_notify_service():
+    """
+    runs notification service of subscribers on a set interval
+    """
+    while True:
+        await notify_subscribers()
+        await asyncio.sleep(60 * 60)  # notify subscriber every 60 minutes
+
+
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--download", action="store_true")
     parser.add_argument("--notify", action="store_true")
+    parser.add_argument("--start", action="store_true")
 
     args = parser.parse_args()
 
@@ -83,6 +100,12 @@ async def main():
     if args.notify:
         print("Starting notification service...")
         await notify_subscribers()
+
+    if args.start:
+        print("Starting download and notification background service...")
+        task_1 = asyncio.create_task(start_background_download_service())
+        task_2 = asyncio.create_task(start_background_notify_service())
+        await asyncio.wait([task_1, task_2])
 
 
 if __name__ == "__main__":
